c479695f7006ade710d9e4ec348777a55f412f39
GhostRoot <31356491+GhstRt@users.noreply.github.com>
2019-12-03 20:49:55 +0300

    Add files via upload

---
+++ undercover-fix.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+rm /usr/share/kali-undercover/scripts/desktopconfig.py && cp ./desktopconfig.py /usr/share/kali-undercover/scripts/ && echo "\nBu Fix KernelBlog.org Tarafından Hazırlanmıştır. İyi Çalışmalar. :)\n"
